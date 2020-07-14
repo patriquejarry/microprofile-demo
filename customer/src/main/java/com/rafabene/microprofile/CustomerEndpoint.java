5ecfb807d1a256ff707da946ece85f70e0ae2773
Rafael Benevides <rafabene@gmail.com>
2020-07-14 11:35:17 -0300

    Fix Tracing implementation

--- customer/src/main/java/com/rafabene/microprofile/CustomerEndpoint.java
+++ customer/src/main/java/com/rafabene/microprofile/CustomerEndpoint.java
@@ -11,6 +11,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -25,6 +26,12 @@ import org.eclipse.microprofile.opentracing.Traced;
 import org.eclipse.microprofile.rest.client.RestClientBuilder;
 
 import io.helidon.microprofile.cors.CrossOrigin;
+import io.helidon.webserver.ServerRequest;
+import io.opentracing.Span;
+import io.opentracing.util.GlobalTracer;
+
+import static io.helidon.tracing.jersey.client.ClientTracingFilter.CURRENT_SPAN_CONTEXT_PROPERTY_NAME;
+import static io.helidon.tracing.jersey.client.ClientTracingFilter.TRACER_PROPERTY_NAME;
 
 @ApplicationScoped
 @Path("/api")
@@ -32,15 +39,17 @@ import io.helidon.microprofile.cors.CrossOrigin;
 public class CustomerEndpoint {
 
 	@Inject
-	@ConfigProperty(name = "preference.api.url",
-			defaultValue = "http://localhost:8081/")
+	@ConfigProperty(name = "preference.api.url", defaultValue = "http://localhost:8081/")
 	private String preferenceURL;
+
+	@Context
+	private ServerRequest serverRequest;
 
 	private static final String RESPONSE_STRING_FORMAT = "customer => %s\n";
 
 	@OPTIONS
 	@CrossOrigin
-	public void doOptions(){
+	public void doOptions() {
 	}
 
 	@GET
@@ -49,27 +58,19 @@ public class CustomerEndpoint {
 	@Operation(description = "Get Customer, Preference and Recommendation")
 	public Response doGet() throws MalformedURLException {
 		URL url = new URL(preferenceURL);
-		PreferenceService preferenceService = RestClientBuilder
-				.newBuilder()
-				.baseUrl(url)
-				.register(PreferenceExceptionWrapper.class)
-				.build(PreferenceService.class);
+		PreferenceService preferenceService = RestClientBuilder.newBuilder().baseUrl(url)
+				.property(TRACER_PROPERTY_NAME, GlobalTracer.get())
+				.property(CURRENT_SPAN_CONTEXT_PROPERTY_NAME, serverRequest.spanContext().get())
+				.register(PreferenceExceptionWrapper.class).build(PreferenceService.class);
 		try {
 			return Response.ok(format(RESPONSE_STRING_FORMAT, preferenceService.getPreference())).build();
 		} catch (PreferenceException e) {
-			return Response
-					.status(Response.Status.SERVICE_UNAVAILABLE)
-					.entity(format(RESPONSE_STRING_FORMAT,
-							format("Error: %d - %s", e.getStatus(), e.getMessage()))
-					)
+			return Response.status(Response.Status.SERVICE_UNAVAILABLE)
+					.entity(format(RESPONSE_STRING_FORMAT, format("Error: %d - %s", e.getStatus(), e.getMessage())))
 					.build();
 		} catch (Exception e) {
-			return Response
-					.status(Response.Status.SERVICE_UNAVAILABLE)
-					.entity(format(RESPONSE_STRING_FORMAT,
-							format("Error: %s", e.getCause()))
-					)
-					.build();
+			return Response.status(Response.Status.SERVICE_UNAVAILABLE)
+					.entity(format(RESPONSE_STRING_FORMAT, format("Error: %s", e.getCause()))).build();
 
 		}
 	}
